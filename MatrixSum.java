a765e02757f240cddfba4e4c51483cf5ed1114bd
Athirark96 <95646145+Athirark96@users.noreply.github.com>
2022-03-23 11:10:06 +0530

    Update MatrixSum.java

--- MatrixSum.java
+++ MatrixSum.java
@@ -13,48 +13,49 @@ class MatrixAdd
          int r2 = in.nextInt();
          System.out.println("Enter the array2 column size : ");
          int c2 = in.nextInt();
-         int a[][] = new int[10][10];
-         int b[][] = new int[10][10];
-          int c[][] = new int[10][10];
+         if(r1==r2 &&c1==c2)
+        {
+            int a[][] = new int[10][10];
+            int b[][] = new int[10][10];
+            int c[][] = new int[10][10];
 
-         System.out.println("Array1 Elements :");
-         for(int i=0;i<r1;i++)
-         {
-             for(int j=0;j<c1;j++)
-             {
+             System.out.println("Array1 Elements :");
+            for(int i=0;i<r1;i++)
+            {
+                for(int j=0;j<c1;j++)
+                {
                    a[i][j] = in.nextInt();
-              }
-         }
+                }
+             }
         
-        System.out.println("Array2 Elements :");
-         for(int i=0;i<r2;i++)
-         {
-             for(int j=0;j<c2;j++)
-             {
+             System.out.println("Array2 Elements :");
+            for(int i=0;i<r2;i++)
+            {
+               for(int j=0;j<c2;j++)
+               {
                    b[i][j] = in.nextInt();
+                }
               }
-         }
-         System.out.println("Matrix 1  : ");
-         for(int i=0;i<r1;i++)
-        {
-            for(int j=0;j<c1;j++)
-            {
-                System.out.print("\t"+a[i][j]+" ");
-            }
-            System.out.println(" ");
-        }  
+             System.out.println("Matrix 1  : ");
+             for(int i=0;i<r1;i++)
+             {
+                 for(int j=0;j<c1;j++)
+                 {
+                     System.out.print("\t"+a[i][j]+" ");
+                 }
+                 System.out.println(" ");
+             }  
 
-       System.out.println("Matrix 2  : ");
-         for(int i=0;i<r2;i++)
-        {
-            for(int j=0;j<c2;j++)
-            {
-                System.out.print("\t"+a[i][j]+" ");
-            }
-            System.out.println(" ");
-        } 
-         if(r1==r2 && c1==c2)
-        {
+              System.out.println("Matrix 2  : ");
+              for(int i=0;i<r2;i++)
+              {
+                  for(int j=0;j<c2;j++)
+                  {
+                      System.out.print("\t"+a[i][j]+" ");
+                   }
+                   System.out.println(" ");
+               } 
+        
             for(int i=0;i<r1;i++)
             {
                 for(int  j=0;j<c1;j++)
@@ -62,13 +63,8 @@ class MatrixAdd
                      c[i][j] = a[i][j] + b[i][j];
                 }
             }
-         }
-        else 
-       {
-               System.out.println("Not possible!!");
-       } 
-      
-        System.out.println("Sum of two Matrix : ");
+    
+          System.out.println("Sum of two Matrix : ");
          for(int i=0;i<r1;i++)
         {
             for(int j=0;j<c1;j++)
@@ -77,6 +73,12 @@ class MatrixAdd
             }
             System.out.println(" ");
         }  
+    }
+     else 
+       {
+               System.out.println("Not possible! as matrix roW and column are not same!");
+       } 
+      
          
     }
 }
